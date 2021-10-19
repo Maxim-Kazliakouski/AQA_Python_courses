22ddad3a4a4487b420b22c56350d248de7589177
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2021-10-20 00:59:35 +0300

    adding 2nd variant for home work(simple)

--- 2nd_variant(simple).py
+++ 2nd_variant(simple).py
@@ -1,7 +1,15 @@
+"""
+The small method for second variant of home work
+"""
 from random import randint
 
 
 def entering_number(number=int(input('Please, enter the number from 1 to 5: '))):
+    """
+    This method uses for choosing number from random list from 1 to 5
+    :param number: it's a number, which user enters from keyboard
+    :return:
+    """
     random_value = randint(1, 5)
     if number == random_value:
         print(f'You are guess the number!\nIt was number {random_value}')
