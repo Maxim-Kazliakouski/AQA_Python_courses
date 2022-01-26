6ac737a330038e4c66ef760b25b1857278369378
Maxim-Kazliakouski <75146035+Maxim-Kazliakouski@users.noreply.github.com>
2022-01-27 02:30:29 +0300

    Update test_writing_allure_report.py

--- tests/test_writing_allure_report.py
+++ tests/test_writing_allure_report.py
@@ -1,6 +1,6 @@
-# import os
+import os
 
 
-# def test_start_allure_report():
-#     print('\nGenerating allure report...')
-#     os.system('allure serve results/')
+def test_start_allure_report():
+    print('\nGenerating allure report...')
+    os.system('allure serve /Users/maxkazliakouski/.jenkins/workspace/test_jenkins/allure-results')
